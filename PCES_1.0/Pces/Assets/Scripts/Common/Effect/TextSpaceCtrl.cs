eb0fa19cf7b5dbfced4b6d6c9ff7a012ae8a53ad
xzp <3094997376@qq.com>
2019-07-28 17:37:00 +0800

    ui字体修改

---
+++ PCES_1.0/Pces/Assets/Scripts/Common/Effect/TextSpaceCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextSpaceCtrl : MonoBehaviour {
+
+    public float fontSpace = 6;
+	// Use this for initialization
+	void Start () {
+        Text[] txt=transform.GetComponentsInChildren<Text>(true);
+        for (int i = 0; i < txt.Length; i++)
+        {
+            TextSpacing ts=txt[i].GetComponent<TextSpacing>();
+            if (ts==null)
+            {
+               ts=txt[i].gameObject.AddComponent<TextSpacing>();
+            }
+            ts._textSpacing = fontSpace;
+        }
+	}		
+}
